621227001901849753ded9d620fd6144d42b3340
Waldemar Porscha <wp@porscha.eu>
2023-11-25 21:43:50 +0100

    Use Kommentar as Module (abstract control)
    - Move "Kommentar" to Common
    - Move .js to Common
    - Create Kommentar-Module in Common

--- src/Logikmodul.script.js
+++
@@ -1,7 +0,0 @@
-function newline(device, online, progress, context) {
-    // input contains c++ like control chars (\n,\\)
-    var text = device.getParameterByName(context.textbox);
-    var replaced = text.value.split("\\n").join("\n");
-    text.value = replaced;
-}
-
