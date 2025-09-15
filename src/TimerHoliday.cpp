5e30d8d99563d303b283bbe5755f2ad547754d2c
Waldemar Porscha <wp@porscha.eu>
2025-09-15 09:59:09 +0200

    Remove some debug output in TimerHoliday

--- src/TimerHoliday.cpp
+++ src/TimerHoliday.cpp
@@ -65,7 +65,7 @@ void TimerHoliday::updateDate(tm mNow, bool sendHolidays /* = true */)
     // year changed => month changed => day change
     if (mYearTick != mNow.tm_year)
     {
-        logDebugP("tick: year %d -> %d", mYearTick, mNow.tm_year);
+        // logDebugP("tick: year %d -> %d", mYearTick, mNow.tm_year);
 
         calculateEaster(mNow.tm_year + 1900);
         calculateAdvent(mNow.tm_year);
@@ -75,19 +75,19 @@ void TimerHoliday::updateDate(tm mNow, bool sendHolidays /* = true */)
     }
     if (mMonthTick != mNow.tm_mon)
     {
-        logDebugP("tick: month %d -> %d", mMonthTick, mNow.tm_mon);
+        // logDebugP("tick: month %d -> %d", mMonthTick, mNow.tm_mon);
 
         mMonthTick = mNow.tm_mon;
         mDayTick = -1;
     }
     if (mDayTick != mNow.tm_mday)
     {
-        logDebugP("tick: day %d -> %d", mDayTick, mNow.tm_mday);
+        // logDebugP("tick: day %d -> %d", mDayTick, mNow.tm_mday);
 
         const bool holidaysChanged = calculateHolidays(mNow.tm_year, mNow.tm_mon + 1, mNow.tm_mday);
         if (holidaysChanged && sendHolidays)
         {
-            logDebugP("send holidays: %d, %d", holidayToday(), holidayTomorrow());
+            // logDebugP("send holidays: %d, %d", holidayToday(), holidayTomorrow());
             sendHoliday();
         }
 
